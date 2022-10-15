cc2acff9a58f19af1b34ab06adde0ca78e59df02
Guo Cheng <guocheng@Guos-MacBook-Pro.local>
2022-10-15 00:29:44 -0400

    bob

--- koh/src/App.js
+++ koh/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+//firebase stuff
+import React from "react";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 function App() {
+  const firebaseApp = firebase.apps[0];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React & Firebase</h1>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
     </div>
   );
 }
+
+
+
 
 export default App;
--- koh/src/index.js
+++ koh/src/index.js
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// firebase stuffnthings
+const firebaseConfig = {
+  apiKey: "AIzaSyCkjLgGLMPTAqQPDM0cBYCCg4yJSBoiD2E",
+  authDomain: "koh-firebase-d7492.firebaseapp.com",
+  projectId: "koh-firebase-d7492",
+  storageBucket: "koh-firebase-d7492.appspot.com",
+  messagingSenderId: "470225001028",
+  appId: "1:470225001028:web:23d41e3a9f5fae29c07a11",
+  measurementId: "G-V24V7DPFLX"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db =  firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth, db }
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
