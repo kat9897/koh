0ebad8a77b5f22c89d1fe1da2b9c89b8d8f4628f
Guo Cheng <guocheng@Guos-MacBook-Pro.local>
2022-10-15 05:42:52 -0400

    Added habits list to users

--- koh/src/App.js
+++ koh/src/App.js
@@ -6,7 +6,7 @@ import React from "react";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, child, push, update, get, onValue } from "firebase/database";
 
 function writeUserData(userId, email, phone, birthday, display, user_name, pass_word) {
   const db = getDatabase();
@@ -17,9 +17,13 @@ function writeUserData(userId, email, phone, birthday, display, user_name, pass_
     user_phone: phone,
     user_birthday: birthday,
     display_name: display,
-    user_id: userId
+    user_id: userId,
+    habits: [11,12]
   });
 }
+
+
+
 
 function addHabit(userId, habit_id, title, body, color, day, start, end, prog ) {
   const db = getDatabase();
@@ -34,7 +38,12 @@ function addHabit(userId, habit_id, title, body, color, day, start, end, prog ) 
     end_time: end,
     imp_or_comp: prog
   });
+  //Pull all habits from userId
+  //const habitRef = ref(db, 'users/' + userId + '/habits')
+  
+  //append habit_id to array
 
+  //update '\users' + userId + '/habits' with new array
 }
 
 
@@ -43,6 +52,8 @@ function App() {
   writeUserData(1, "griffin@mail.com", 1234567890, "10/12/2001", "Griffin St-Maurice", "griffin", "griffin")
   writeUserData(2, "katerina@mail.com", 9059049526, "09/19/2001", "Katerina Best", "kat9877", "kat's password")
   addHabit(1, 11, "Work on KOH frontend", "Develop the front end and make it presentable given the 36hrs.", "#FF3409", "Friday", 2200, 2359, "imp")
+  
+
   
   return (
     <div>
