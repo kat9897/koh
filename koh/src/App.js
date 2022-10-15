e04818dca4587ae0c15c24e2b18f92cba290f3a1
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2022-10-15 02:06:29 -0400

    Firebase working 2

--- koh/src/App.js
+++ koh/src/App.js
@@ -1,112 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase/compat/app';
 
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Header } from './components/Header'
-import { Users } from './components/Users'
-import { DisplayBoard } from './components/DisplayBoard'
-import CreateUser from './components/CreateUser'
-import { getAllUsers, createUser } from './services/UserService'
+import React from 'react';
 
-/* Old App
 function App() {
+  const firebaseApp = firebase.apps[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React & Firebase</h1>
+      <h2>By @farazamiruddin</h2>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
     </div>
   );
-}
-*/
-
-const firebaseApp = firebase.apps[0];
-
-class App extends Component {
-
-  state = {
-    user: {},
-    users: [],
-    numberOfUsers: 0
-  }
-
-  createUser = (e) => {
-      createUser(this.state.user)
-        .then(response => {
-          console.log(response);
-          this.setState({numberOfUsers: this.state.numberOfUsers + 1})
-      });
-  }
-
-  getAllUsers = () => {
-    getAllUsers()
-      .then(users => {
-        console.log(users)
-        this.setState({users: users, numberOfUsers: users.length})
-      });
-  }
-
-  onChangeForm = (e) => {
-      let user = this.state.user
-      if (e.target.name === 'firstname') {
-          user.firstName = e.target.value;
-      } else if (e.target.name === 'lastname') {
-          user.lastName = e.target.value;
-      } else if (e.target.name === 'email') {
-          user.email = e.target.value;
-      }
-      this.setState({user})
-  }
-
-  render() {
-    
-    return (
-      <div className="App">
-        <Header></Header>
-        <div className="container mrgnbtm">
-          <div className="row">
-            <div className="col-md-8">
-                <CreateUser 
-                  user={this.state.user}
-                  onChangeForm={this.onChangeForm}
-                  createUser={this.createUser}
-                  >
-                </CreateUser>
-            </div>
-            <div className="col-md-4">
-                <DisplayBoard
-                  numberOfUsers={this.state.numberOfUsers}
-                  getAllUsers={this.getAllUsers}
-                >
-                </DisplayBoard>
-            </div>
-          </div>
-        </div>
-        <div>
-          <h1>React & Firebase</h1>
-          <code>
-            <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
-          </code>
-        </div>
-        <div className="row mrgnbtm">
-          <Users users={this.state.users}></Users>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default App;
