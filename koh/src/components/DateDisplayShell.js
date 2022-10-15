02b6190aee12a401bdcd4b54dff8b188dc34a08b
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2022-10-15 19:13:47 -0400

    got dates to display for days!! Not much implemented

--- koh/src/components/DateDisplayShell.js
+++ koh/src/components/DateDisplayShell.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, {Text} from 'react'
+import DateRow from './DateRow'
 
-export const DateDisplayShell = () => {
-    return
+export const DateDisplayShell = ({ date }) => {
+    return (
+        <>
+            {/* TODO call backend api */}
+            <DateRow date="1" checks={[true, true, false, true, false]} highlight="Highlight" dailyLog="Description of my day"/>
+        </>
+    )
 }
--- koh/src/components/Webpage.js
+++ koh/src/components/Webpage.js
@@ -5,11 +5,33 @@ import { HabitDisplayShell } from './HabitDisplayShell'
 import { DateDisplayShell } from './DateDisplayShell'
 
 export const Webpage = () => {
+    const currDate = new Date().getDate();
     return (
         <div>
+            <HabitDisplayShell/>
+            {/* 7 days */}
+            <DateDisplayShell
+                date={currDate}
+            />
+            <DateDisplayShell
+                date={currDate+1}
+            />
+            <DateDisplayShell
+                date={currDate+2}
+            />
+            <DateDisplayShell
+                date={currDate+3}
+            />
+            <DateDisplayShell
+                date={currDate+4}
+            />
+            <DateDisplayShell
+                date={currDate+5}
+            />
+            <DateDisplayShell
+                date={currDate+6}
+            />
             <Header></Header>
-            <HabitDisplayShell></HabitDisplayShell>
-            <DateDisplayShell></DateDisplayShell>
         </div>
     );
 }
